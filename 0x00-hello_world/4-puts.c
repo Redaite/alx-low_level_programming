96107849c0a1a636fdc6de1634ce0480a5922e53
Redaite <redaitekalgeteye@gmail.com>
2022-06-03 13:20:54 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
-#include <stdio.h>
-
+#include <stdio>
 /**
  * main - Entry point
  *
@@ -8,7 +6,6 @@
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle \n")
-
-    	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
