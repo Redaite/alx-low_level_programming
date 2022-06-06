4e66f91873546cdc06c577fec9936d390c289691
Redaite <redaitekalgeteye@gmail.com>
2022-06-06 14:36:47 +0300

    The success combination in business is

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*main -> a program that print all possible different combination of three digit
+*Return: always 0
+**/
+int main(void)
+{
+int ch;
+int n;
+int l;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 49; n <= 57; n++)
+{
+for (l = 50; l <= 57; n++)
+{
+if (n > ch && l > n)
+{
+putchar(ch);
+putchar(n);
+putchar(l);
+if (ch != 55 || n != 56 || l != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar(10); /**This is ascii code for a new line **/
+return (0);
+}
+
