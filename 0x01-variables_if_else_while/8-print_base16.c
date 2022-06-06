861bb07072b57807ebf0b1a136d1f7e1c5647730
Redaite <redaitekalgeteye@gmail.com>
2022-06-06 12:45:28 +0300

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdio.>
+#include <stdio.h>
 
 /**
 *main -> assign a random number to the variable n each time it is excuted
@@ -7,16 +7,22 @@
 */
 int main(void)
 {
+/**
+*
+* hexadecimal
+*0 1 2 3 4 5 6 7 8 9 a b c d e f
+**/
 char ch;
 int n;
-for (n = 48; n <=57; n++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 }
-char ch;
-for (ch = 'a; ch <= 'f'; ch++)
-{
-if (ch != 'q' && ch != 'e')
+for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
 }
+putchar(10);
+return (0);
+}
+
