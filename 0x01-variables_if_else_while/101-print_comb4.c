ee9b7788bea70008edc30de6b94831425e1158ff
Redaite <redaitekalgeteye@gmail.com>
2022-06-09 22:39:15 +0300

    The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,29 +5,26 @@
 */
 int main (void)
 {
-int ch;
+int i;
+for (i = 48; i < 56; i++)
+{
 int n;
-int l;
-for (ch = 48; ch <= 57; ch++)
+for (n = i + 1; n < 57; n++)
 {
-for (n = 49; n <= 57; n++)
+int j;
+for (j = n + 1; j <= 58; j++)
 {
-for (l = 50; l <= 57; n++)
-{
-if (n > ch && l > n)
-{
-putchar(ch);
+putchar(i);
 putchar(n);
-putchar(l);
-if (ch != 55 || n != 56 || l != 57)
-{
+putchar(j);
+if (i == 55 && n == 56 && j == 57)
+continue;
 putchar(44);
 putchar(32);
 }
 }
 }
-}
-}
+
 putchar(10); /**This is ascii code for a new line **/
 return (0);
 }
